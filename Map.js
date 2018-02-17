0eb6da2160388ae55ce0bd104ddedc29d9a2a22c
revpaulbeard <36569387+revpaulbeard@users.noreply.github.com>
2018-02-17 18:51:12 +0000

    Add files via upload

---
+++ Map.js
@@ -0,0 +1,21 @@
+jsXML = '<sbentries>'
+ + ' '
++ '<sbhead iconscale = "50"  sbcolwidth = "25%"  showpolys = "Y"  shownumbers = "Y"  sborder = "N"  initzoom = "auto"  maptype = "G_NORMAL_MAP"  baselocation = "Trafalgar Square&lt;BR&gt;London"  baselatlng = "51.5080, -0.12807"  forwhat = "set" />'
+ + ' '
++ 
+
+	'<sbentry label="Group 1" forecolor="#000000" backcolor="#ffffff" group="0" sbindex="-1" SVGPath="Person"/>'
+ + ' '
++ 
+	'<sbentry label="Frankie and Jonnie Armadillo" forecolor="" backcolor="" group="0" sbindex="1" infoafteraddress="01234 567890  fussball@pigsblabber.com " address="Buckingham Palace, London, "/>'
+ + ' '
++ 
+	'<sbentry label="Group 2" forecolor="#ffffff" backcolor="#0000ff" group="1" sbindex="-1" SVGPath="Church"/>'
+ + ' '
++ 
+	'<sbentry label="BS Methodist Church" forecolor="" backcolor="" group="1" sbindex="2" LatLng="52.2233, -0.26687" infoafteraddress="01480 454665 07777 777777  Speciality Pastries" address="10 Berkley Street, St Neots, Cambridgeshire, PE19 2NB"/>'
+ + ' '
++ 
+'</sbentries>'
+ + ' '
++ ' '
